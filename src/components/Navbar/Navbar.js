aa202757fcf0ad1267eeb409e0f74da2d2a1cf90
nicotacchia <nico.tacchia@hotmail.com.ar>
2022-02-06 12:29:19 -0300

    first commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
       <nav className="conteiner">
        <div className="navBar">
          <img src={'./images/imgcircuito.jpg'} className="logo" alt='Logo'/>  
-       
+      
        <ul>
            <li><a href="a">Gimnasio</a></li>
            <li><a href="a">Deportes </a></li>
